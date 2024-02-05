597f5bde45698bc4ea8ea51d17a8e1687e740b6c
Maurot11 <mauro.co.uk@hotmail.com>
2024-02-05 11:17:42 +0000

    Backend connected

--- client/main.js
+++ client/main.js
@@ -1,8 +1,8 @@
 const form = document.getElementById('booking')
 // const baseURL = import.meta.env.VITE_ServerURL
 
- form.addEventListener('submit', async function handleBooking(event) {
-    event.preventDefault()
+function handleBooking(e) {
+    e.preventDefault()
 
     const formData = new FormData(form)
     const bookings = Object.fromEntries(formData)
@@ -10,7 +10,7 @@ const form = document.getElementById('booking')
     console.log(bookings)
     // if (bookings.name == '' || bookings.phoneNo == '')
 
-    const response = await fetch(`http://localhost:8080/booking`, {
+    fetch(`http://localhost:8080/booking`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -18,11 +18,6 @@ const form = document.getElementById('booking')
         body: JSON.stringify({bookings})
     })
 
-    if (response.ok) {
-        console.log('OKAY')
-    } else {
-        console.error('ERROR')
-    }
-})
+}
 
-// form.addEventListener('submit', async function handleBooking)
+form.addEventListener('submit', handleBooking)
