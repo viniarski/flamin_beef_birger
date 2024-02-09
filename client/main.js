517fc6e15d69830896087d8d0750b98ab5a9cf56
Michal Winiarski <41969647+viniarski@users.noreply.github.com>
2024-02-09 14:14:44 +0000

    remove vite

--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
 const form = document.getElementById('booking');
 const reviewForm = document.getElementById('reviewForm');
-// const baseURL = 'http://localhost:8080';
-const baseURL = import.meta.env.VITE;
+const baseURL = 'https://flamin-beef-burger-server.onrender.com';
+// const baseURL = import.meta.env.VITE_ServerURL;
 const getReviewsDiv = document.getElementById('getReviewsDiv');
 
 function handleBooking(e) {
